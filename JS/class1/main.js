b804731f80b17ad864c17c75f75771e0f4a30116
aman_goyal <amangoyal2339@gmail.com>
2023-12-08 02:41:30 +0530

    class1_practise

---
+++ JS/class1/main.js
@@ -0,0 +1,16 @@
+let a = 1;
+a=2;
+console.log(a);
+let apple = [1,"apple",3,4,5];
+console.log(apple);
+
+const user1 = {
+    firstnam: "lamo",
+    secondname: "asdasd"
+}
+
+console.log(user1["firstnam"]);
+
+function sum(a,b){
+    return a+b;
+}
